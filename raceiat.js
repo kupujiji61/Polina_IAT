a8f3af0dc95e572413402563e8cef5a19d3dbd2b
kupujiji61 <130174574+kupujiji61@users.noreply.github.com>
2025-12-09 19:55:00 +0300

    Update raceiat.js

--- raceiat.js
+++ raceiat.js
@@ -44,47 +44,48 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
             stimulusCss : {color:'#31940F','font-size':'2.3em'}
         },
         
-    attribute1 : { // ГОРЫ
-        name : 'Горы',
-        title : {
-            media : { word : 'Горы' },
-            css   : { color:'#0000FF', 'font-size':'1.8em' },
-            height: 4
-        },
-   
-        stimulusMedia : [
-            { image: 'https://drive.google.com/uc?export=view&id=1kCgE0FDzuYbB12SF2p7LHMS-LI9TUqjU' },
-            { image: 'https://drive.google.com/uc?export=view&id=1Z1v7UA9GBywXRcs5DEM27awERnZgfAqr' },
-            { image: 'https://drive.google.com/uc?export=view&id=1X3TAQuDL-ZLlYPG-ev8_fsXnkIW27nLg' },
-            { image: 'https://drive.google.com/uc?export=view&id=15_lszf1VhzKG4JD0SaT3zMmjTHqU63jq' },
-            { image: 'https://drive.google.com/uc?export=view&id=16CZGPlhj9wgY9LH20GTpMyGx7YAQYBWv' },
-            { image: 'https://drive.google.com/uc?export=view&id=1shiQ3DED2h1C_v4ZNuccd_pDe4PkPczK' },
-            { image: 'https://drive.google.com/uc?export=view&id=1t6MNteFUAfRAUJCylDWEAOixNuPR8u4k' },
-            { image: 'https://drive.google.com/uc?export=view&id=1UuVcMYPsUU0PCzyh36XHtSitYotUEVNk' }
-        ],
-        stimulusCss : { color:'#0000FF', 'font-size':'2.3em' }
-    },
-        attribute2 : {
-            name : 'Болото',
-            title : {
-                media : {word : 'Болото'},
-                css : {color:'#0000FF','font-size':'1.8em'},
-                height : 4 //Used to position the "Or" in the combined block.
+            attribute1 : {
+                name : 'Горы',
+                title : {
+                    media : { word : 'Горы' },
+                    css   : { color:'#0000FF','font-size':'1.8em' },
+                    height: 4
+                },
+                stimulusMedia : [
+                    { word: 'гора 1' },
+                    { word: 'гора 2' },
+                    { word: 'гора 3' },
+                    { word: 'гора 4' },
+                    { word: 'гора 5' },
+                    { word: 'гора 6' },
+                    { word: 'гора 7' },
+                    { word: 'гора 8' }
+                ],
+                stimulusCss : { color:'#0000FF','font-size':'2.3em' }
             },
-            
-            stimulusMedia : [
-            { image: 'https://drive.google.com/uc?export=view&id=1c98HLM6ymW-XIXkhgJqIy3gCqlpFqaA-' },
-            { image: 'https://drive.google.com/uc?export=view&id=1-tsdDRGHophVM_sHAHnsE6ZaqOJLlSj2' },
-            { image: 'https://drive.google.com/uc?export=view&id=1pHzghJPLAop4Blt0SbsHNB1FyXEyzEMS' },
-            { image: 'https://drive.google.com/uc?export=view&id=1ytjjUpX1L68wYEdUAxCFBJKLPUtEqL5l' },
-            { image: 'https://drive.google.com/uc?export=view&id=1CNQ_SBxSVo-mAdXahpyphe06_rViNl-n' },
-            { image: 'https://drive.google.com/uc?export=view&id=1BPLMX3hjogFt46TK7I2RjgdgNhgCkUgl' },
-            { image: 'https://drive.google.com/uc?export=view&id=1HjrpwkM9NtCnTFmf7kuupIiEG_F0H891' },
-            { image: 'https://drive.google.com/uc?export=view&id=1bud3Ccs08KDljxb0yvI_5a4-9iJwZFBe' }
-        ],
-            //Stimulus css
-            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-        },
+
+            // Атрибут 2: Болота (пока как слова)
+            attribute2 : {
+                name : 'Болота',
+                title : {
+                    media : { word : 'Болота' },
+                    css   : { color:'#0000FF','font-size':'1.8em' },
+                    height: 4
+                },
+                stimulusMedia : [
+                    { word: 'болото 1' },
+                    { word: 'болото 2' },
+                    { word: 'болото 3' },
+                    { word: 'болото 4' },
+                    { word: 'болото 5' },
+                    { word: 'болото 6' },
+                    { word: 'болото 7' },
+                    { word: 'болото 8' }
+                ],
+                stimulusCss : { color:'#0000FF','font-size':'2.3em' }
+            },
+
+        
         base_url : {//Where are your images at?
             image : ""
         },
